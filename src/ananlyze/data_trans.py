e28f64990bf3ada1988ef1ea21bbaaaa0fc779c3
fsrmmomo <1906994189@qq.com>
2022-12-13 21:09:10 +0800

    添加置信度区间绘画

--- src/ananlyze/data_trans.py
+++ src/ananlyze/data_trans.py
@@ -1,9 +1,9 @@
 import os
 import pickle as pkl
 import struct
-
+from scipy import stats
 from numpy.ma import mean
-
+import numpy as np
 from util.utils import *
 
 file_num = 200
@@ -24,10 +24,100 @@ def get_node_list():
     return node_list
 
 
-def read_raw():
-    read_file_dir = "../../result/DivSketch/"
+def error_cul():
+    #
+    # read_file_dir = "../../result/TM-SALFI/"
+    # all_dict = pkl_read("../../result/data_dict/all.pkl")
+    # for k,v in all_dict.items():
+    #     print(k)
+    #     print(v)
+    # print(all_dict)
+
+
+    read_file_dir = "../../result/TM-SALFI/"
 
     node_list = get_node_list()
+
+
+    data_dict = dict()
+
+    for file in os.listdir(read_file_dir):
+        data_dict[file] = dict()
+        s_dict = data_dict[file]
+        for file_no in range(10):
+            s_dict[file_no] = dict()
+            for node in node_list:
+                s_dict[file_no][node] = [0 for _ in range(data_point_num)]
+
+
+        read_file_name = read_file_dir + file
+        print(read_file_name)
+        x = [0 for _ in range(data_point_num)]
+        with open(read_file_name, "r") as rf:
+            for i in range(file_num):
+                start = rf.readline()
+                string = start.split("\\")[-1]
+                node = string.split("-")[-1].split(".")[0]
+                dat_file_no = int(string.split("-")[0])
+                for j in range(data_point_num):
+                    nums = float(rf.readline())
+                    s_dict[dat_file_no][node][j] = nums
+        # print(s_dict[0]["access1"])
+        # print(s_dict)
+
+
+    for k, s_dict in data_dict.items():
+        tmp_dict = dict()
+        for dat_file_no in range(dat_num):
+            tmp_dict[dat_file_no] = dict()
+            new_dict = tmp_dict[dat_file_no]
+            new_dict["access"] = [0 for _ in range(data_point_num)]
+            new_dict["merge"] = [0 for _ in range(data_point_num)]
+            new_dict["core"] = [0 for _ in range(data_point_num)]
+            # print(type(s_dict))
+            for node in node_list:
+                key = node[:-1]
+                if key == "core":
+                    down = 4
+                else:
+                    down = 8
+                for j in range(data_point_num):
+                    new_dict[key][j] += s_dict[dat_file_no][node][j] / down
+        data_dict[k] = tmp_dict
+        print(data_dict[k])
+    # with open("../../result/data_dict/all.pkl", "wb") as wf:
+    #     pkl.dump(data_dict, wf)
+
+    error_data = dict()
+    for k, s_dict in data_dict.items():
+        error_data[k] = dict()
+        new_dict = error_data[k]
+
+        node_set = ["access", "merge", "core"]
+        for node in node_set:
+            new_dict[node] = []
+            for i in range(data_point_num):
+                datas = [s_dict[j][node][i] for j in range(dat_num)]
+                new_dict[node].append(cul_up_and_down(datas,0.95))
+                # for file_no in range(dat_num):
+        print(error_data[k])
+
+    save_dict = dict()
+    save_dict["TM-SALFI"] = error_data
+    print(save_dict)
+    pkl_write("../../result/data_dict/error_1.pkl",save_dict)
+
+def cul_up_and_down(datas, Confidence=0.95):
+    x = np.array(datas)
+    return stats.t.interval(Confidence,len(datas)-1,x.mean(),x.std())
+
+
+def read_raw():
+    read_file_dir = "../../result/TM-SALFI/"
+
+    node_list = get_node_list()
+
+
 
     data_dict = dict()
     for file in os.listdir(read_file_dir):
@@ -75,7 +165,7 @@ def aggregation_data():
             for j in range(data_point_num):
                 new_dict[key][j] += s_dict[node][j] / down
         data_dict[k] = new_dict
-    with open("../../result/data_dict/DivSketch.pkl", "wb") as wf:
+    with open("../../result/data_dict/TM-SALFI.pkl", "wb") as wf:
         pkl.dump(data_dict, wf)
     print(data_dict["ARE"])
     print(data_dict["HHD_ARE"])
@@ -184,4 +274,5 @@ if __name__ == '__main__':
     # read_raw()
     # aggregation_data()
     # count_num()
-    get_CDF()
+    # get_CDF()
+    error_cul()
--- src/plot/draw.py
+++ src/plot/draw.py
@@ -37,7 +37,8 @@ def draw_line(x: list, y_list: list, label_list: list, color_list: list,
     x_size = len(x)
 
     plt.figure(num=no, figsize=figsize, dpi=300)
-    plt.rcParams['font.sans-serif'] = ['SimSun','Times New Roman']
+    plt.rcParams['font.sans-serif'] = ['Times New Roman']
+    # plt.rcParams['font.sans-serif'] = ['SimSun','Times New Roman']
     # plt.rcParams['axes.unicode_minus'] = False  # 用来正常显示负号(无用)
 
     # 修改刻度线向内 需在plot之前
@@ -67,7 +68,7 @@ def draw_line(x: list, y_list: list, label_list: list, color_list: list,
         ax.get_yaxis().get_offset_text().set(va='bottom', ha='left',fontsize="large",
                                              fontproperties="stixgeneral",fontstyle="normal")
 
-    plt.xlabel(xlabel, fontsize=18)
+    plt.xlabel(xlabel, fontdict=font1)
     plt.ylabel(ylabel, fontdict=font1)
     plt.legend(prop=font1)
     plt.grid()
@@ -160,9 +161,11 @@ def plot_linear(task, ylimit: list, title_patter: str,sci=False,log=False ):
                   label_list=label_list, xticks_label=xticks_label, yticks_labels=None,
                   color_list=color_list, marker_list=marker_list,
                   xlimit=[0, data_point_num - 1], ylimit=ylimit,
-                  xlabel="存储空间(MB)", ylabel=ylabel,
+                  xlabel="Memory Usage (MB)", ylabel=ylabel,
                   figsize=(8, 4.5), title=title,
                   save_name=save_name, no=i,sci=sci,log=log)
+
+
 
 
 def draw_bar_no_cmp(x: list, y: list, xticks_label: list,
@@ -185,10 +188,14 @@ def draw_bar_no_cmp(x: list, y: list, xticks_label: list,
     # zorder=0 设置图层级别 不遮挡数据
     plt.grid(zorder=0)
 
+    color_list = []
+    color_list.extend(['g' for _ in range(8)])
+    color_list.extend(['b' for _ in range(8)])
+    color_list.extend(['c' for _ in range(4)])
     #使用对数曲线
     if log:
         plt.yscale('log')
-    plt.bar(x, y, width=0.4, zorder=10,color = "g")
+    plt.bar(x, y, width=0.4, zorder=10,color = color_list)
         # plt.bar(x, y, color=color_list[i], label=label_list[i], marker=marker_list[i])
 
     plt.yticks(fontproperties='Times New Roman', size=18, ha="right")
@@ -198,7 +205,6 @@ def draw_bar_no_cmp(x: list, y: list, xticks_label: list,
 
     # 设置文字和刻度的间距,防止左下角x和y重叠
     # ax.tick_params(axis="x", pad=10)
-    # ax.set_xticklabels(xlabel, rotation=45, ha='right')
     # 设置纵坐标科学计数法
     if sci:
         ax.ticklabel_format(style='sci', scilimits=(0,0), axis='y',useMathText=True)
@@ -223,7 +229,104 @@ def draw_bar_no_cmp(x: list, y: list, xticks_label: list,
         f.savefig("../../result/figure/" + save_name + ".svg", dpi=300, format="svg")
 
 # plot_bar():
+def draw_error_bar(x: list, y_list: list,e_list:list, label_list: list, color_list: list,
+              yticks_labels: list, xticks_label: list,
+              marker_list: list, xlimit, ylimit, xlabel: str, ylabel: str,
+              figsize: tuple, title: str,
+              save_name: str, no: int,
+              sci=False,log=False):
+    assert len(x) == len(xticks_label)
+    assert len(y_list) == len(label_list)
+    assert len(y_list)<=len(marker_list)
+    assert len(y_list)<=len(color_list)
+    x_size = len(x)
 
+    plt.figure(num=no, figsize=figsize, dpi=300)
+    plt.rcParams['font.sans-serif'] = ['Times New Roman']
+    # plt.rcParams['font.sans-serif'] = ['SimSun','Times New Roman']
+    # plt.rcParams['axes.unicode_minus'] = False  # 用来正常显示负号(无用)
+
+    # 修改刻度线向内 需在plot之前
+    plt.rcParams['xtick.direction'] = 'in'
+    plt.rcParams['ytick.direction'] = 'in'
+
+    plt.title(title, fontdict=font1)
+
+    #使用对数曲线
+    if log:
+        plt.yscale('log')
+    # plot 曲线
+    for i, y in enumerate(y_list):
+        print(e_list[i])
+        print(y)
+        plt.errorbar(x,y,yerr=e_list[i],color=color_list[i],ecolor='b',ms=5,mfc='wheat',mec='salmon',capsize=3,
+                     elinewidth=3,label=label_list[i], marker=marker_list[i])
+        # plt.plot(x, y, color=color_list[i], label=label_list[i], marker=marker_list[i])
+
+    plt.yticks(fontproperties='Times New Roman', size=18, ha="right")
+    plt.xticks(ticks=np.linspace(x[0], x[-1], x_size), labels=xticks_label, fontproperties='Times New Roman', size=18)
+
+    ax = plt.gca()
+    # 设置文字和刻度的间距,防止左下角x和y重叠
+    ax.tick_params(axis="x", pad=10)
+
+    # 设置纵坐标科学计数法
+    if sci:
+        ax.ticklabel_format(style='sci', scilimits=(0,0), axis='y',useMathText=True)
+        print(mpl.rcParams['xtick.labelsize'])
+        ax.get_yaxis().get_offset_text().set(va='bottom', ha='left',fontsize="large",
+                                             fontproperties="stixgeneral",fontstyle="normal")
+
+    plt.xlabel(xlabel, fontdict=font1)
+    plt.ylabel(ylabel, fontdict=font1)
+    plt.legend(prop=font1)
+    plt.grid()
+    if xlimit is not None:
+        plt.xlim(xlimit[0], xlimit[1])
+    if ylimit is not None:
+        plt.ylim(ylimit[0], ylimit[1])
+    plt.tight_layout()
+
+    f = plt.gcf()
+    plt.show()
+    if save_name is not None:
+        f.savefig("../../result/figure/" + save_name + ".jpg", dpi=300)
+        f.savefig("../../result/figure/" + save_name + ".svg", dpi=300, format="svg")
+
+def plot_error_bar(task, ylimit: list, title_patter: str,sci=False,log=False ):
+    cmp_list = cmp_dict[task]
+    label_list = cmp_list
+    for i, node in enumerate(node_set):
+        y_list = []
+        e_list = []
+
+        for algo in cmp_list:
+
+            yt_list = data_dict[algo][task][node][:data_point_num]
+            et_list = error_dict[algo][task][node][:data_point_num]
+            # et_list.append(error_dict[algo][task][node][:data_point_num])
+            # print(error_dict[algo][task][node][:data_point_num])
+            # print(et_list)
+            et_list = [[yt_list[x]- et_list[x][0] for x in range(data_point_num)], [et_list[x][1]-yt_list[x] for x in range(data_point_num)]]
+            # print(et_list)
+            y_list.append(yt_list)
+            e_list.append(et_list)
+        # et_list = [[et_list[x][0] for x in range(data_point_num)], [et_list[x][1] for x in range(data_point_num)]]
+        # title = "Heavy Hitter Detection on " + node.capitalize() + " Node"
+        title = title_patter + " on " + node.capitalize() + " Node"
+        save_name = task + "_" + node + "_error"
+        if task == "CE":
+            ylabel = "RE"
+        else:
+            ylabel = task
+        print(e_list)
+        draw_error_bar(x=list(range(data_point_num)), y_list=y_list,e_list=e_list,
+                  label_list=label_list, xticks_label=xticks_label, yticks_labels=None,
+                  color_list=color_list, marker_list=marker_list,
+                  xlimit=[0, data_point_num - 1], ylimit=ylimit,
+                  xlabel="Memory Usage (MB)", ylabel=ylabel,
+                  figsize=(8, 4.5), title=title,
+                  save_name=save_name, no=i,sci=sci,log=log)
 
 def get_node_list():
     node_list = []
@@ -235,23 +338,44 @@ def get_node_list():
         node_list.append("core" + str(i))
     return node_list
 
-def plot_statistics():
-    global data_dict
-    for algo in algo_set:
-        data_dict[algo] = pkl_read(data_dict_dir + algo + ".pkl")
-    data_dict.update(pkl_read("../../result/data_dict/output.pkl"))
+def plot_statistics_error_bar():
 
-    # for k,v in data_dict.items():
-    #     print(k)
-    #     print(v)
-    # print(data_dict[algo])
-    # print(data_dict)
-    # plot_linear("ARE", ylimit=[0, 2], title_patter="Flow Size Measurement")
-    # plot_linear("HHD_ARE", ylimit=[0, 0.03], title_patter="Heavy Hitter Detection")
-    # plot_linear("HHD_F1", ylimit=[0.9, 1], title_patter="Heavy Hitter Detection")
-    # plot_linear("CE", ylimit=[0.00001,0.1], title_patter="Cardinality Estimation",log=True)
-    # plot_linear("CE", ylimit=[0,0.1], title_patter="Cardinality Estimation",log=False)
-    # plot_linear("WMRE", ylimit=[0.0001,2], title_patter="Flow Size Distribution Estimation",log=True)
+    global data_dict,error_dict
+    data_dict = pkl_read("../../result/data_dict/all_data.pkl")
+    error_dict = pkl_read("../../result/data_dict/all_error.pkl")
+
+    plot_error_bar("ARE", ylimit=[0, 2], title_patter="Flow Size Measurement")
+    # plot_error_bar("HHD_ARE", ylimit=[0, 0.03], title_patter="Heavy Hitter Detection")
+    # plot_error_bar("HHD_F1", ylimit=[0.9, 1], title_patter="Heavy Hitter Detection")
+    # plot_error_bar("CE", ylimit=[0.00001,0.1], title_patter="Cardinality Estimation",log=True)
+    # plot_error_bar("CE", ylimit=[0,0.1], title_patter="Cardinality Estimation",log=False)
+    # plot_error_bar("WMRE", ylimit=[0.0001,2], title_patter="Flow Size Distribution Estimation",log=True)
+
+def plot_statistics():
+    # global data_dict
+    # for algo in algo_set:
+    #     data_dict[algo] = pkl_read(data_dict_dir + algo + ".pkl")
+    # data_dict.update(pkl_read("../../result/data_dict/output.pkl"))
+    # print(data_dict.keys())
+    # data_dict["TM-SALFM"] = data_dict["ServerCount Sketch"]
+    # data_dict.pop("ServerCount Sketch")
+    # print(data_dict.keys())
+    #
+    #
+    #
+    # pkl_write("../../result/data_dict/all_data.pkl", data_dict)
+    global data_dict
+    data_dict = pkl_read("../../result/data_dict/all_data.pkl")
+
+
+
+
+    plot_linear("ARE", ylimit=[0, 2], title_patter="Flow Size Measurement")
+    plot_linear("HHD_ARE", ylimit=[0, 0.03], title_patter="Heavy Hitter Detection")
+    plot_linear("HHD_F1", ylimit=[0.9, 1], title_patter="Heavy Hitter Detection")
+    plot_linear("CE", ylimit=[0.00001,0.1], title_patter="Cardinality Estimation",log=True)
+    plot_linear("CE", ylimit=[0,0.1], title_patter="Cardinality Estimation",log=False)
+    plot_linear("WMRE", ylimit=[0.0001,2], title_patter="Flow Size Distribution Estimation",log=True)
 
 def plot_flow_and_pkt_count():
     flow_count_dict = pkl_read("../../result/data_dict/flow_count_per_node_dict.pkl")
@@ -267,13 +391,13 @@ def plot_flow_and_pkt_count():
     print(len(xticks_label))
     draw_bar_no_cmp(x=list(range(20)), y=flow_count_list,
                     xlimit=[-1, 20], ylimit=[0,90000],xticks_label=node_list,
-                    xlabel="Node Name", ylabel="Number of Packets",
+                    xlabel="Node Name", ylabel="Number of Flows",
                     figsize=(9, 4.5), title="Number of Flows on Each Node",
                     save_name="Flows_Number", no=1, sci=True, log=False)
 
     draw_bar_no_cmp(x=list(range(20)), y=pkt_count_list,
                     xlimit=[-1, 20], ylimit=[0,500000],xticks_label=node_list,
-                    xlabel="Node Name", ylabel="Number of Flows",
+                    xlabel="Node Name", ylabel="Number of Packets",
                     figsize=(9, 4.5), title="Number of Packets on Each Node",
                     save_name="Packets_Number", no=1, sci=True, log=False)
 
@@ -288,12 +412,31 @@ def plot_CDF():
               xlabel="Percentage of Top Flow(%)", ylabel="Percentage of Packets(%)",
               figsize=(8, 4.5), title="Cumulative Distribution Function of Flow Size",
               save_name="CDF", no=1, sci=False, log=False)
+
+
+def get_all_data():
+    global data_dict
+    data_dict["TM-SALFI"] = pkl_read("../../result/data_dict/TM-SALFI.pkl")
+    data_dict.update(pkl_read("../../result/data_dict/output_10.pkl"))
+
+    print(data_dict.keys())
+    data_dict["TM-SALFM"] = data_dict["ServerCount Sketch"]
+    data_dict.pop("ServerCount Sketch")
+    global error_dict
+    error_dict = pkl_read("../../result/data_dict/error_1.pkl")
+    error_dict.update(pkl_read("../../result/data_dict/confidence_interval.pkl"))
+    error_dict["TM-SALFM"] = error_dict["ServerCount Sketch"]
+    error_dict.pop("ServerCount Sketch")
+    print(error_dict.keys())
+    pkl_write("../../result/data_dict/all_data.pkl", data_dict)
+    pkl_write("../../result/data_dict/all_error.pkl", error_dict)
+
 if __name__ == '__main__':
 
-    global data_dict
-    for algo in algo_set:
-        data_dict[algo] = pkl_read(data_dict_dir + algo + ".pkl")
-    data_dict.update(pkl_read("../../result/data_dict/output.pkl"))
+    # global data_dict
+    # for algo in algo_set:
+    #     data_dict[algo] = pkl_read(data_dict_dir + algo + ".pkl")
+    # data_dict.update(pkl_read("../../result/data_dict/output.pkl"))
 
     # for k,v in data_dict.items():
     #     print(k)
@@ -308,4 +451,5 @@ if __name__ == '__main__':
     # plot_linear("WMRE", ylimit=[0.0001,2], title_patter="Flow Size Distribution Estimation",log=True)
 
     # plot_flow_and_pkt_count()
-
+    plot_statistics()
+    # plot_statistics_error_bar()
